8741c841d028145525b2f8c59df72e6a1576f333
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-09-12 13:39:48 -0300

    Cleaning the project archives

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { View, StyleSheet, Text } from 'react-native'
+import { Container, Title } from './styles'
+
+
+export function Groups(){
+  return (
+    <Container>
+      <Title>Thamires</Title>
+    </Container>
+  )
+}
